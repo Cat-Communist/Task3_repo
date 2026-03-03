b8f4d2a00bc9d05bb4a457967b2b68a652d0aa71
Cat-Communist <popovvp2502@gmail.com>
2026-03-04 00:55:10 +0800

    Suddenly done the whole task

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Common;
+using System.Linq;
+
+namespace Task3
+{
+    public class Logic
+    {
+        public static string CalcFractions(string inputString)
+        {
+            Dictionary<char, int> dictionary = new();
+            foreach (var c in inputString.ToLower())
+            {
+                if (dictionary.ContainsKey(c) && Char.IsLetter(c))
+                    dictionary[c]++;
+                else if (Char.IsLetter(c))
+                    dictionary[c] = 1;
+            }
+            
+            var totalLetter = dictionary.Values.Sum();
+
+            var outputMsg = "";
+            foreach (var pair in dictionary)
+                outputMsg += $"{pair.Key} = {(float)pair.Value / totalLetter * 100:F2}%\n";
+
+            return outputMsg;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var result = Logic.CalcFractions(input);
+
+            Console.WriteLine(result);
+        }
+    }
+}
